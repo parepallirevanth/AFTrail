a36a05d56e8aabd4f4d186b96f845b946e7a4c39
“parepallirevanth” <parepallirevanth@gmail.com>
2020-07-19 16:10:00 +0530

    modified flaskapi

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -1,6 +1,5 @@
 #sudo apt-get update
 cd $(dirname $0)
 cd ../
-#comment
 #rebuild Dockerfiles and runing
 sudo docker-compose up -d --build
--- fastapi/app/main.py
+++ fastapi/app/main.py
@@ -1,6 +1,8 @@
+
 from typing import Optional
 
 from fastapi import FastAPI
+import os
 
 app = FastAPI()
 
@@ -9,7 +11,15 @@ app = FastAPI()
 def read_root():
     return {"Hello": "World"}
 
-
-@app.get("/items/{item_id}")
+@app.get("/items/{item_id}/{q}")
 def read_item(item_id: int, q: Optional[str] = None):
-    return {"item_id": item_id, "q": q}
+   open('U_%s.py' % q, 'w').write("k") # create a new file with userid
+   os.system("cp temp.txt U_* ")             # copy dag content in it
+   os.system("sed -i \"s/VAR/NewDag/g\" U_*")
+   os.system("cp U_* dags/")		     # copy user dag to dags folder
+   return {"item_id": item_id, "q": q}
+
+#open('U_%s.py' % item_id, 'w').write("k")
+#os.system("cp tuto.py U_* ")
+#os.system("sed 's/\"U_/\"U_/g' U_*")
+
